968ad27761e2b84fd6e40bf054c0b65572f330a4
yosefrow <yosefrow@gmail.com>
2019-03-13 22:53:20 +0000

    update build script to automatically use .env.example in worst case scenario

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -xe
 
+ScriptDir=$(dirname $0)
+cd $ScriptDir
+
+if [ ! -f ./.env ]; then
+    echo ".env file not found. Copying .env.example to .env"
+    cp -i .env.example .env
+fi
+
 docker-compose down
 docker-compose up -d
